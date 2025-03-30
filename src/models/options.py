3946e996ef4d21e134deb8987d28addf42c1d782
@ThViviani <developsviviani@gmail.com>
2025-03-30 22:40:23 +1000

    added special parameter for the learning rate decay.

--- src/models/options.py
+++ src/models/options.py
@@ -9,17 +9,19 @@ class TrainOptions:
                  identity_lambda=0,
                  buffer_size=50,
                  n_epochs_decay=100,
-                 n_epochs=100):
+                 n_epochs=100,
+                 last_epoch_in_prev_experiment=100):
         """Construct a train option.
         Parameters:
-            lr (float)                  -- the learning rate for optimization solver
-            l1_lambda (float)           -- the weight of l1_loss for generator train
-            betas (tuple[float, float]) -- hyperparameters for momentum in the Adam optimizer
-            cyc_cycle_lambda (float)    -- the weight of cycle_loss for generators train in the CycleGAN
-            identity_lambda (float)     -- the weight of identity_loss for a generators train step in the CycleGAN
-            buffer_size (int)           -- the size of image buffer that stores previously generated images
-            n_epochs_decay (int)        -- the number of epochs over which the learning rate will linearly decay to zero
-            n_epochs (int)              -- the number of epochs with the initial learning rate before it starts to decay.
+            lr (float)                          -- the learning rate for optimization solver
+            l1_lambda (float)                   -- the weight of l1_loss for generator train
+            betas (tuple[float, float])         -- hyperparameters for momentum in the Adam optimizer
+            cyc_cycle_lambda (float)            -- the weight of cycle_loss for generators train in the CycleGAN
+            identity_lambda (float)             -- the weight of identity_loss for a generators train step in the CycleGAN
+            buffer_size (int)                   -- the size of image buffer that stores previously generated images
+            n_epochs_decay (int)                -- the number of epochs over which the learning rate will linearly decay to zero
+            n_epochs (int)                      -- the number of epochs with the initial learning rate before it starts to decay
+            last_epoch_in_prev_experiment (int) -- the number of last epoch in the prev experiment of the training.
         """
         
         self.lr              = lr
@@ -29,4 +31,5 @@ class TrainOptions:
         self.identity_lambda = identity_lambda
         self.buffer_size     = buffer_size
         self.n_epochs_decay  = n_epochs_decay
-        self.n_epochs        = n_epochs 
+        self.n_epochs        = n_epochs
+        self.last_epoch      = last_epoch_in_prev_experiment 
